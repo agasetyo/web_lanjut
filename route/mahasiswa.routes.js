5b821d45e18d28ce4fddbb6dff53b3bff5a6abd6
livyathanvy <indy25.mhs@akb.ac.id>
2026-06-03 12:46:48 +0700

    Update mahasiswa.routes.js

--- route/mahasiswa.routes.js
+++ route/mahasiswa.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from 'multer';
-import { authenticateToken } from "../middleware/VerifyToken.js
+import { authenticateToken } from "../middleware/VerifyToken.js;
 const router = express.Router();
 const upload = multer();
 
