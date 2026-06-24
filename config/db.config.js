dbf6b20d8d38095958493a2a4930d3921b514132
livyathanv <indy25.mhs@akb.ac.id>
2026-06-24 08:23:25 +0700

    fixing vercel

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,6 @@
 import { Sequelize } from "sequelize";
 import mysql2 from "mysql2";
 
-<<<<<<< HEAD
 const db = new Sequelize(process.env.database_url,{
     dialect : "mysql",
     dialectModule : mysql2,
@@ -19,17 +18,3 @@ export default db;
 // (async()=>{
 //      await db.sync();
 // })();
-=======
-const db = new Sequelize('matkul_web_lanjut', 'root', '',{
-    host :"localhost",
-    dialect : "mysql",
-    "define": {
-        "timestamps": false
-    }
-});
-export default db;
-
-(async()=>{
-    await db.sync();
-})();
->>>>>>> 5de5b1ef5b71941e13a15f1b57a26377eab18028
